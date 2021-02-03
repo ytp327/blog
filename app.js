eaaaa4fea2b41199ecbace0a6653a5dc794d0ffb
tianpei yang <1464439923@qq.com>
2021-02-03 00:17:30 -0500

    Google login

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan =  require('morgan')
 const exphbs = require('express-handlebars')
+const passport =  require('passport')
+const session= require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({path:'./config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -17,15 +22,29 @@ if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
-//Handlebars
+// Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
+
+// Sessions
+app.use(
+    session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
